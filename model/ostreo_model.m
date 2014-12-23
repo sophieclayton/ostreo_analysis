fa5dfa5656fcc7f6d6f4413e8cfd4dffaecf9755
Sophie Clayton <sclayton@uw.edu>
2014-12-22 21:39:20 -0800

    model now runs all three experiments

--- model/ostreo_model.m
+++ model/ostreo_model.m
@@ -1,39 +1,84 @@
-function [output] = ostreo_model(par);
+function [output] = ostreo_model;
+
+% parameters for all experiments
+dy = 0.05;
+y = 33.2:dy:34.4; % m
+k = 5e-08; % diffusity coeff in salinity space, S2/s
 
 % time stepping
 dt = 30; % time step in seconds
 tend = 10;
 tstep = 1:dt:60*60*24*tend;
 
-OI=zeros(length(y),length(tstep));
-OII=zeros(length(y),length(tstep));
+OI_null=zeros(length(y),length(tstep));
+OII_null=zeros(length(y),length(tstep));
+
+OI_pos=zeros(length(y),length(tstep));
+OII_pos=zeros(length(y),length(tstep));
+
+OI_neg=zeros(length(y),length(tstep));
+OII_neg=zeros(length(y),length(tstep));
 
 % initial conditions and boundary conditions
-OI(1,:)=5000;
-OII(end,:)=5000;
+OI_null(1,:)=5000;
+OII_null(end,:)=5000;
 
-% OI(1,2:end)=0.1;
-% OII(end,2:end)=0.1;
+OI_pos(1,:)=5000;
+OII_pos(end,:)=5000;
 
-% set gaussian nutrient profile
-%k=10^4n0=5;
-% n=0.5 + n0*exp(-((y-15000).^2)./(2*5000^2));
-% umax = 1/(60*60*24);
-% u1=(umax.*(n./(n+k1)));
-% u2=u1;
-%n=ones(length(y),length(tstep));
+OI_neg(1,:)=5000;
+OII_neg(end,:)=5000;
+
+% EXPERIMENT 1, unet = 0
+u1 = zeros(length(y));
+u2 = u1;
+
+m1 = 0;
+m2 = m1;
 
 for t = 2:length(tstep);
-%    n0=5*(1+sin((2*pi*1/(60*60*24*3))*tstep(t)));
-%    n(:,t)=0.5 + n0*exp(-((y-15000).^2)./(2*2000^2));
-%    u1=(umax.*(n(:,t)./(n(:,t)+k1)));
-%    u2=u1;
     for ystep = 2:length(y)-1;
-        OI(ystep,t) = OI(ystep,t-1) + (k/(dy^2)*dt*(OI(ystep+1,t-1)-2*OI(ystep,t-1)+OI(ystep-1,t-1))+u1(ystep)*dt*OI(ystep,t-1))-m1*dt*OI(ystep,t-1);
-        if OI(ystep,t)<0, OI(ystep,t)=0; end
-        OII(ystep,t) = OII(ystep,t-1) + (k/(dy^2)*dt*(OII(ystep+1,t-1)-2*OII(ystep,t-1)+OII(ystep-1,t-1))+u2(ystep)*dt*OII(ystep,t-1))-m2*dt*OII(ystep,t-1);
-        if OII(ystep,t)<0, OII(ystep,t)=0; end
+        OI_null(ystep,t) = OI_null(ystep,t-1) + (k/(dy^2)*dt*(OI_null(ystep+1,t-1)-2*OI_null(ystep,t-1)+OI_null(ystep-1,t-1))+u1(ystep)*dt*OI_null(ystep,t-1))-m1*dt*OI_null(ystep,t-1);
+        if OI_null(ystep,t)<0, OI_null(ystep,t)=0; end
+        OII_null(ystep,t) = OII_null(ystep,t-1) + (k/(dy^2)*dt*(OII_null(ystep+1,t-1)-2*OII_null(ystep,t-1)+OII_null(ystep-1,t-1))+u2(ystep)*dt*OII_null(ystep,t-1))-m2*dt*OII_null(ystep,t-1);
+        if OII_null(ystep,t)<0, OII_null(ystep,t)=0; end
+    end
+end
+clear u1 u2 m1 m2
+
+% EXPERIMENT 2, unet > 0
+u1 = 0.9/(60*60*24).*ones(length(y));
+u2 = u1;
+
+m1 =0.1/(60*60*24);
+m2 = m1;
+
+for t = 2:length(tstep);
+    for ystep = 2:length(y)-1;
+        OI_pos(ystep,t) = OI_pos(ystep,t-1) + (k/(dy^2)*dt*(OI_pos(ystep+1,t-1)-2*OI_pos(ystep,t-1)+OI_pos(ystep-1,t-1))+u1(ystep)*dt*OI_pos(ystep,t-1))-m1*dt*OI_pos(ystep,t-1);
+        if OI_pos(ystep,t)<0, OI_pos(ystep,t)=0; end
+        OII_pos(ystep,t) = OII_pos(ystep,t-1) + (k/(dy^2)*dt*(OII_pos(ystep+1,t-1)-2*OII_pos(ystep,t-1)+OII_pos(ystep-1,t-1))+u2(ystep)*dt*OII_pos(ystep,t-1))-m2*dt*OII_pos(ystep,t-1);
+        if OII_pos(ystep,t)<0, OII_pos(ystep,t)=0; end
+    end
+end
+clear u1 u2 m1 m2
+
+% EXPERIMENT 3, unet < 0
+u1 = zeros(length(y));
+u2 = u1;
+
+m1 = 0.1/(60*60*24);
+m2 = m1;
+
+for t = 2:length(tstep);
+    for ystep = 2:length(y)-1;
+        OI_neg(ystep,t) = OI_neg(ystep,t-1) + (k/(dy^2)*dt*(OI_neg(ystep+1,t-1)-2*OI_neg(ystep,t-1)+OI_neg(ystep-1,t-1))+u1(ystep)*dt*OI_neg(ystep,t-1))-m1*dt*OI_neg(ystep,t-1);
+        if OI_neg(ystep,t)<0, OI_neg(ystep,t)=0; end
+        OII_neg(ystep,t) = OII_neg(ystep,t-1) + (k/(dy^2)*dt*(OII_neg(ystep+1,t-1)-2*OII_neg(ystep,t-1)+OII_neg(ystep-1,t-1))+u2(ystep)*dt*OII_neg(ystep,t-1))-m2*dt*OII_neg(ystep,t-1);
+        if OII_neg(ystep,t)<0, OII_neg(ystep,t)=0; end
     end
 end
 
-output = [OI(:,tend) OII(:,tend)];
+output.null = [OI_null(:,t) OII_null(:,t)];
+output.pos = [OI_pos(:,t) OII_pos(:,t)];
+output.neg = [OI_neg(:,t) OII_neg(:,t)];
--- model/simple_ostreo_S.m
+++ model/simple_ostreo_S.m
@@ -11,40 +11,6 @@ load ../data/ostreo % load the ostreo clade abundance data
 
 kuro=find(lon>140 & z==0); % use only the data from the Kuroshio
 
-% set up domain and set parameters
-% SET UP GLOBAL PARAMETERS FOR THINGS THAT DON"T CHANGE BETWEEN MODEL RUNS
-
-dy = 0.05;
-y = 33.2:dy:34.4; % m
-k =5e-08; % diffusity coeff in salinity space, S2/s
-% k = 0.25*10^4; % diffusivity coeff, m2/s
-% u1 =*10^-5.*ones(length(y)); % s-1
-% u2 =u1;
-%u2 = 1.1*10^-5.*exp(-((y-34).^2)./(2*1^2));
-%u1 = 1*10^-5.*exp(-((y-33.8).^2)./(2*0.5^2));
-%u1=0.9/(60*60*24).*ones(length(y));
-u1=zeros(length(y));
-u2=u1;
-m1=0.1/(60*60*24);
-%m1=0;
-m2=m1;
-
-par = [];
-
-out = ostreo_model(par);
+out = ostreo_model;
 
 run('../figures/plot_ostreo_model.m')
-
-
-
-%
-%     nit=nitrate(ZZ==0 & stn<41);
-%     
-%    figure(2);
-%    [AX,H1,H2] = plotyy(SALT,nit,y,u1,'plot'); set(AX,'FontSize',14,'XColor','k','YColor','k');
-%    set(H1,'LineStyle','none','Marker','o','MarkerEdgeColor','k');
-%    set(H2,'LineWidth',2,'Color','k')
-%    % plot(y,u1,'k','LineWidth',2);axis([33.2 34.4 0 1.2*10^-5]);xlabel('salinity','FontSize',14);ylabel('\mu (day^{-1})','FontSize',14);set(gca,'FontSize',14);
-% set(get(AX(1),'Ylabel'),'String','NO_3 + NO_2 (mmol m^{3})','FontSize',14,'Color','k')
-% set(get(AX(2),'Ylabel'),'String','\mu (day^{-1})','FontSize',14,'Color','k')
-% % end
